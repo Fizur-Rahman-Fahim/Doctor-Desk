6ddef45f43be6f075b52760882af5d34075cbc5b
Fizur-Rahman-Fahim <fizurrahman733433@gmail.com>
2025-06-11 21:04:29 +0600

    all domain alowed

--- doctor_desk/settings.py
+++ doctor_desk/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG')
 
-ALLOWED_HOSTS = ['https://doctor-desk-okum.onrender.com','https://doctor-desk-eight.vercel.app/', 'doctor-desk-eight.vercel.app', 'localhost', '127.0.0.1']
+ALLOWED_HOSTS = ['*','https://doctor-desk-okum.onrender.com','https://doctor-desk-eight.vercel.app/', 'doctor-desk-eight.vercel.app', 'localhost', '127.0.0.1']
 
 
 # Application definition
